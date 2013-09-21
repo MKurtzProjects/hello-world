21de7f5f15be13ad39629338b44d19bde4d06d48
Michael Kurtz <michaelkurtz10@gmail.com>
2013-09-21 14:08:57 -0400

    Created a Current_Time file
    This file explicitly refers to project I made about changing colors by
    time of day

--- logic.php
+++ logic.php
@@ -1,5 +1,20 @@
 <?php
+date_default_timezone_set('America/New_York');
 
-	$age = 10;
+    $day_name = date('l');
+	$day_numeric = date('d');
+	$month = date('F');
+	$year = date('Y');
+	
+	$hour = date('g');
+	$minute = date('i');
+	$time_label = date('a');
+	//$hour_24 is not displayed to users, is a 24 hour clock that let's me write easier if statements.  See below.
+	$hour_24 = date ('G');
 
-?>
+	$color = "#374D61";
+
+	if ($hour_24 >= 6 AND $hour_24 <=20) {
+		$color = "#91CBFF";
+		return $color;
+	}
