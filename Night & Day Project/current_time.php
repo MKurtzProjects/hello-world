d640be0796ff296d29eebc5c73f1d09dc67ff6ed
Michael Kurtz <michaelkurtz10@gmail.com>
2013-09-22 10:07:19 -0400

    Added new folder for Night&Day project for better organization

--- Night & Day Project/current_time.php
+++ Night & Day Project/current_time.php
@@ -9,20 +9,21 @@
        body {
            background-color:<?=$color?>
         }
+    
+    <title> Weather Update </title>
+
 
 </style>
 </head>
 
 
 <body>
-	 
+
 <!--The below code displays time -->
 
 	<h1> <?=$hour?>:<?=$minute?><?=$time_label?> </h1>
 	<h2> <?=$day_name?>, <?=$month?> <?=$day_numeric?>, <?=$year?> </h2>
 
-
-<?=$time?>
 
 </body>
 
